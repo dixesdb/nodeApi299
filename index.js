2bf80aabca2495be1adc25c3848074aaf63d7bf7
Daniel Benavides <daiel97dot2@gmail.com>
2020-02-05 11:53:26 -0500

    Segundo commit

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+require('./config/config');
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./classes/server"));
+const express = require('express');
 const mongoose_1 = __importDefault(require("mongoose"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
@@ -12,26 +13,26 @@ const post_1 = __importDefault(require("./routes/post"));
 const cors_1 = __importDefault(require("cors"));
 const usuarioP_1 = __importDefault(require("./routes/usuarioP"));
 const posttienda_1 = __importDefault(require("./routes/posttienda"));
-const server = new server_1.default();
+const app = express();
 // Body parser
-server.app.use(body_parser_1.default.urlencoded({ extended: true }));
-server.app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
 // FileUpload
-server.app.use(express_fileupload_1.default({ useTempFiles: true }));
+app.use(express_fileupload_1.default({ useTempFiles: true }));
 //configurar cors
-server.app.use(cors_1.default({ origin: true, credentials: true }));
+app.use(cors_1.default({ origin: true, credentials: true }));
 // Rutas de mi app
-server.app.use('/user', usuario_1.default);
-server.app.use('/userP', usuarioP_1.default);
-server.app.use('/posts', post_1.default);
-server.app.use('/postsT', posttienda_1.default);
+app.use('/user', usuario_1.default);
+app.use('/userP', usuarioP_1.default);
+app.use('/posts', post_1.default);
+app.use('/postsT', posttienda_1.default);
 // Conectar DB
-mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+mongoose_1.default.connect('mongodb+srv://admin:peluditos@cluster0-3o4h8.mongodb.net/PeluditosDB?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }, (err) => {
     if (err)
         throw err;
     console.log('Base de datos ONLINE');
 });
 // Levantar express
-server.start(() => {
-    console.log(`Servidor corriendo en puerto ${server.port}`);
+app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor corriendo puerto ${process.env.PORT} `);
 });
